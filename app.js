3aa27f17de5bd1512ba0ef4976e20e3366f21de8
Jake Whelan <jake.whelan@citi.com>
2019-01-24 15:38:53 +0000

    feat: add unix and HTTPS support

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const fastifyAutoLoad = require('fastify-autoload')
 const fastifyCookie = require('fastify-cookie')
 const cosmiconfig = require('cosmiconfig')
 const explorer = cosmiconfig('resolvr')
+const fs = require('fs')
 
-module.exports = (fastify, opts, next) => {
+const getConfig = async () => {
+  const { config } = await explorer.search()
+  return config
+}
+
+const mainPlugin = async (fastify, opts, next) => {
   fastify.register(fastifyAutoLoad, {
     dir: path.join(__dirname, 'services')
   })
@@ -14,12 +20,27 @@ module.exports = (fastify, opts, next) => {
     if (err) throw err
   })
 
-  explorer.search().then(async ({ config }) => {
-    const nginxConfigWorker = await createNginxConfigWorker(config)
-    nginxConfigWorker.listen(stdout => {
-      console.log(stdout)
-    })
+  const config = await getConfig()
+  const nginxConfigWorker = await createNginxConfigWorker(config)
+  nginxConfigWorker.listen(stdout => {
+    console.log(stdout)
   })
 
   next()
 }
+
+const start = async () => {
+  const {
+    'key-path': keyPath,
+    'cert-path': certPath
+  } = await getConfig()
+
+  const options = keyPath
+    ? { https: { key: fs.readFileSync(keyPath), cert: fs.readFileSync(certPath) } }
+    : {}
+  const fastify = require('fastify')(options)
+
+  fastify.register(mainPlugin)
+}
+
+start()
